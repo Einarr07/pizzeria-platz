e732ce29a2cbee0d68244fd6748172bc11aa28d1
Einarr07 <aidenperse@gmail.com>
2025-07-22 11:31:07 -0500

    Pageable
    - The new format for the presentation of the data was added through the pagination.

---
+++ src/main/java/com/platzi/pizza/persistence/repository/IPizzaPagSortRepository.java
@@ -0,0 +1,11 @@
+package com.platzi.pizza.persistence.repository;
+
+import com.platzi.pizza.persistence.entity.PizzaEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.ListPagingAndSortingRepository;
+
+public interface IPizzaPagSortRepository extends ListPagingAndSortingRepository<PizzaEntity, Integer> {
+
+    Page<PizzaEntity> findByAvailableTrue(Pageable pageable);
+}
--- src/main/java/com/platzi/pizza/service/PizzaService.java
+++ src/main/java/com/platzi/pizza/service/PizzaService.java
@@ -1,11 +1,15 @@
 package com.platzi.pizza.service;
 
 import com.platzi.pizza.persistence.entity.PizzaEntity;
+import com.platzi.pizza.persistence.repository.IPizzaPagSortRepository;
 import com.platzi.pizza.persistence.repository.IPizzaRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @Service
@@ -13,14 +17,30 @@ import java.util.List;
 public class PizzaService {
 
     private final IPizzaRepository pizzaRepository;
+    private final IPizzaPagSortRepository pizzaPagSortRepository;
 
-    public List<PizzaEntity> getAll() {
+    /*public List<PizzaEntity> getAll() {
         return pizzaRepository.findAll();
+    }*/
+
+    public Page<PizzaEntity> getAll(int page, int elements) {
+
+        PageRequest pageRequest = PageRequest.of(page, elements);
+        return pizzaPagSortRepository.findAll(pageRequest);
     }
 
+    /*
     public List<PizzaEntity> findAllByAvailableTrueOrderByPrice() {
         pizzaRepository.countByVeganTrue();
         return pizzaRepository.findAllByAvailableTrueOrderByPrice();
+    }
+     */
+    public Page<PizzaEntity> getAvailable(int page, int elements, String sortBy, String sorDirection) {
+        pizzaRepository.countByVeganTrue();
+
+        Sort sort = Sort.by(Sort.Direction.fromString(sorDirection), sortBy);
+        PageRequest pageRequest = PageRequest.of(page, elements, sort);
+        return pizzaPagSortRepository.findByAvailableTrue(pageRequest);
     }
 
     public List<PizzaEntity> getWith(String description) {
